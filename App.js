7ac15f6ec58f27151ff35dea62d345c7b37e3b2d
Yasindev46 <103483006+Yasindev46@users.noreply.github.com>
2022-05-17 11:39:26 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Order from './Components/Order';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Order/>
+    </div>
+  );
+}
+
+export default App;
